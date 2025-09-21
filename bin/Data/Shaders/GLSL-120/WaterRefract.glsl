64f785efe5174f22d2452c2fab3dc93d53f534ac
Okkoman <okkoman0xx0@gmail.com>
2025-09-21 23:23:16 +0200

    Android : updated Water shader

--- bin/Data/Shaders/GLSL-120/WaterRefract.glsl
+++ bin/Data/Shaders/GLSL-120/WaterRefract.glsl
@@ -1,3 +1,5 @@
+//#define LOW_QUALITY
+
 #include "Uniforms.glsl"
 #include "Samplers.glsl"
 #include "Transform.glsl"
@@ -11,18 +13,25 @@ void VS()
     vec3 worldPos = (iPos * iModelMatrix).xyz;
 
     vColor = iColor;
-
     gl_Position = GetClipPos(worldPos);
     vRefractUV = GetQuadTexCoord(gl_Position);
 }
 
 void PS()
 {
+    // refracted color from the refraction map (it's the viewport getted at refract pass : see ForwardUrho2D renderpath)    
+#ifdef LOW_QUALITY
+    vec3 refractedColor = texture2D(sDiffMap, vRefractUV).rgb;
+#else
     const float freq = 35.0;
     const float accel = 5.0;
-    const float amplitude = 0.001; // precision issue with GLES2 (don't decrease this value)
+    const float amplitude = 0.001;
     float variation = amplitude * sin(freq * vRefractUV.x + accel * cElapsedTimePS) + amplitude * cos(freq * vRefractUV.y + accel * cElapsedTimePS);
 
     // refracted color from the refraction map (it's the viewport getted at refract pass : see ForwardUrho2D renderpath)
-    gl_FragColor = vec4(cMatDiffColor.rgb * texture2D(sDiffMap, vRefractUV + vec2(variation)).rgb * vColor.rgb, 1.0);
+    vec3 refractedColor = texture2D(sDiffMap, vRefractUV + vec2(variation)).rgb;
+#endif
+    refractedColor *= cMatDiffColor.rgb;
+    refractedColor *= vColor.rgb;
+    gl_FragColor = vec4(refractedColor, 1.0);
 }
